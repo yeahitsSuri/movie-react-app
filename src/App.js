ba4e1a9541f6a2dd48034455f8582b946d34b0ca
Tianshu Yang <yts9614@gmail.com>
2023-06-23 15:35:09 +0800

    test file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import MovieList from './components/MovieList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [movies, setMovies] = useState([
+    {
+      "Title": "Game of Thrones",
+      "Year": "2011–2019",
+      "imdbID": "tt0944947",
+      "Type": "series",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BYTRiNDQwYzAtMzVlZS00NTI5LWJjYjUtMzkwNTUzMWMxZTllXkEyXkFqcGdeQXVyNDIzMzcwNjc@._V1_SX300.jpg"
+  },
+  {
+      "Title": "Squid Game",
+      "Year": "2021–",
+      "imdbID": "tt10919420",
+      "Type": "series",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BYWE3MDVkN2EtNjQ5MS00ZDQ4LTliNzYtMjc2YWMzMDEwMTA3XkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_SX300.jpg"
+  },
+  {
+      "Title": "Sherlock Holmes: A Game of Shadows",
+      "Year": "2011",
+      "imdbID": "tt1515091",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMTQwMzQ5Njk1MF5BMl5BanBnXkFtZTcwNjIxNzIxNw@@._V1_SX300.jpg"
+  },
+  ])
+  return <div>
+    <MovieList movies = {movies}/>
+  </div>
+};
 
 export default App;
