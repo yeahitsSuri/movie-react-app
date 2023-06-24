2cfb00c99f8aaafc216969520d2e7797dd102410
Alice Liu <liuiman@126.com>
2023-06-24 18:58:15 +0800

    Added popular movies section

--- src/screen/HomePage.js
+++ src/screen/HomePage.js
@@ -4,38 +4,50 @@ import MovieListHeader from '../components/MovieListHeader';
 import AddFavourites from '../components/AddFavourites';
 import RemoveFavourites from '../components/RemoveFavourites';
 
-const HomePage = ({ movies, favourites, addFavouriteMovie, removeFavouriteMovie }) => {
-  const showSearchResultsHeader = movies.length > 0;
+const HomePage = ({movies, favourites, popularMovies, addFavouriteMovie, removeFavouriteMovie}) => {
+    const showSearchResultsHeader = movies.length > 0;
 
-  return (
-    <>
-      {showSearchResultsHeader && (
-        <div className='row d-flex align-items-center mt-3 mb-4'>
-          <MovieListHeader header='Searching Results: '/>
-        </div>
-      )}
+    return (
+        <>
+            {showSearchResultsHeader && (
+                <div className='row d-flex align-items-center mt-3 mb-4'>
+                    <MovieListHeader header='Searching Results: '/>
+                </div>
+            )}
 
-      <div className='row'>
-        <MovieList
-          movies={movies}
-          handleFavouritesClick={addFavouriteMovie}
-          favoriteIcon={AddFavourites}
-        />
-      </div>  
+            <div className='row'>
+                <MovieList
+                    movies={movies}
+                    handleFavouritesClick={addFavouriteMovie}
+                    favoriteIcon={AddFavourites}
+                />
+            </div>
 
-      <div className='row d-flex align-items-center mt-3 mb-4'>
-        <MovieListHeader header='Favourites'/>
-      </div>
+            <div className='row d-flex align-items-center mt-3 mb-4'>
+                <MovieListHeader header='Popular Movies'/>
+            </div>
 
-      <div className='row'>
-        <MovieList
-          movies={favourites}
-          handleFavouritesClick={removeFavouriteMovie}
-          favoriteIcon={RemoveFavourites}
-        />
-      </div>
-    </>
-  );
+            <div className='row'>
+                <MovieList
+                    movies={popularMovies}
+                    handleFavouritesClick={addFavouriteMovie}
+                    favoriteIcon={AddFavourites}
+                />
+            </div>
+
+            <div className='row d-flex align-items-center mt-3 mb-4'>
+                <MovieListHeader header='Favourites'/>
+            </div>
+
+            <div className='row'>
+                <MovieList
+                    movies={favourites}
+                    handleFavouritesClick={removeFavouriteMovie}
+                    favoriteIcon={RemoveFavourites}
+                />
+            </div>
+        </>
+    );
 };
 
 export default HomePage;
