5d7b7973f07d4828ef485dbc20199266e239de9a
Alice Liu <liuiman@126.com>
2023-06-26 00:56:23 +0800

    Moved getMovieFromAPI to itself

--- src/search-page/index.js
+++ src/search-page/index.js
@@ -1,11 +1,27 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import SearchBar from '../components/SearchBar';
 import MovieList from '../components/MovieList';
 import MovieListHeader from '../components/MovieListHeader';
 import AddFavorites from '../components/AddFavorites';
 
-const SearchPage = ({setSearchKey, movies, addFavoriteMovie, setMovies}) => {
+const SearchPage = ({addFavoriteMovie}) => {
+    const [movies, setMovies] = useState([]); // movies from search
+    const [searchKey, setSearchKey] = useState(''); // search key the user uses
     const showSearchResultsHeader = movies.length > 0;
+
+    const getMovieFromAPI = async (searchKey) => {
+        const url = `http://www.omdbapi.com/?s=${searchKey}&apikey=a52e368a`;
+        const response = await fetch(url);
+        const responseJson = await response.json();
+
+        if (responseJson.Search) {
+            setMovies(responseJson.Search);
+        }
+    };
+
+    useEffect(() => {
+        getMovieFromAPI(searchKey);
+    }, [searchKey]);
 
     useEffect(() => {
         setSearchKey(''); // Clear the searchKey when refresh
