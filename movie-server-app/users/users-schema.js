9453bf5d3c684129209345e9f1c4660f80edc71e
Alice Liu <liuiman@126.com>
2023-06-25 20:41:49 +0800

    Changed users schema

--- movie-server-app/users/users-schema.js
+++ movie-server-app/users/users-schema.js
@@ -1,15 +1,21 @@
 import mongoose from "mongoose";
 
 const UsersSchema = new mongoose.Schema({
-                                                  username: String,
-                                                  password: String,
-                                                  role: String,
-                                                  firstName: String,
-                                                  lastName: String,
-                                                  email: String,
-    list: Array // for normal users, it's an array of favorite movies
-    // for admin users, it's an array of movies to recommend on home page
-    // this array contains id of movies
-                                              }, {collection: "users"});
+                                            username: String,
+                                            password: String,
+                                            role: String,
+                                            firstName: String,
+                                            showFirstName: Boolean,
+                                            lastName: String,
+                                            showLastName: Boolean,
+                                            email: String,
+                                            showEmail: Boolean,
+                                            list: Array, // for normal users, it's an array of
+                                                         // favorite movies
+                                            // for admin users, it's an array of movies to
+                                            // recommend on home page this array contains id of
+                                            // movies
+                                            showList: Boolean
+                                        }, {collection: "users"});
 
 export default UsersSchema;
