b4fb0e8d6c598af7894fbb2b9685ee2a378cb098
Alice Liu <liuiman@126.com>
2023-06-25 23:05:24 +0800

    Made web header responsive to currentUser state

--- src/components/WebHeader.js
+++ src/components/WebHeader.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const WebHeader = (props) => {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
-    <div className="col">
-      <h1 className="header-container">
-        <span>{props.header}</span>
-        <Link to="/home" className="btn btn-link home-link">Home</Link>
-        <Link to="/login" className="btn btn-link home-link">Login</Link>
-        <Link to="/register" className="btn btn-link home-link">Register</Link>
-        <Link to="/profile" className="btn btn-link home-link">Profile</Link>
-        <Link to="/search" className="btn btn-link home-link search-link">Search</Link>
-      </h1>
-    </div>
+      <div className="col">
+        <h1 className="header-container">
+          <span>{props.header}</span>
+          <Link to="/home" className="btn btn-link home-link">Home</Link>
+          {currentUser ? (
+              <>
+                <Link to="/profile" className="btn btn-link home-link">Profile</Link>
+                <Link to="/search" className="btn btn-link home-link search-link">Search</Link>
+              </>
+          ) : (
+               <>
+                 <Link to="/login" className="btn btn-link home-link">Login</Link>
+                 <Link to="/register" className="btn btn-link home-link">Register</Link>
+                 <Link to="/search" className="btn btn-link home-link search-link">Search</Link>
+               </>
+           )}
+        </h1>
+      </div>
   );
 };
 
