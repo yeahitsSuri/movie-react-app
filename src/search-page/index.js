4fdda0f0e2b2cf2ab39d24497d8d71c5fb334fb5
Alice Liu <liuiman@126.com>
2023-06-25 23:44:18 +0800

    Fixed format

--- src/search-page/index.js
+++ src/search-page/index.js
@@ -1,45 +1,45 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import SearchBar from '../components/SearchBar';
 import MovieList from '../components/MovieList';
 import MovieListHeader from '../components/MovieListHeader';
 import AddFavorites from '../components/AddFavorites';
 
-const SearchPage = ({ setSearchKey, movies, addFavoriteMovie, setMovies }) => {
-  const showSearchResultsHeader = movies.length > 0;
+const SearchPage = ({setSearchKey, movies, addFavoriteMovie, setMovies}) => {
+    const showSearchResultsHeader = movies.length > 0;
 
-  useEffect(() => {
-    setSearchKey(''); // Clear the searchKey when refresh
-    return () => {
-      setMovies([]); // Clear the movies state when refresh
-    };
-  }, [setSearchKey, setMovies]);
+    useEffect(() => {
+        setSearchKey(''); // Clear the searchKey when refresh
+        return () => {
+            setMovies([]); // Clear the movies state when refresh
+        };
+    }, [setSearchKey, setMovies]);
 
-  return (
-    <div>
-      <div className='row d-flex align-items-center mt-3 mb-2'>
-        <MovieListHeader header='Search Here: ' />
-      </div>
-      <div className='row d-flex align-items-center mt-1 mb-4'>
-        <SearchBar searchKey='' setSearchKey={setSearchKey} />
-      </div>
-
-      {/* Display Searching Results */}
-      {showSearchResultsHeader && (
+    return (
         <div>
-          <div className='row d-flex align-items-center mt-3 mb-4'>
-            <MovieListHeader header='Searching Results: ' />
-          </div>
-          <div className='row'>
-            <MovieList
-              movies={movies}
-              handleFavoritesClick={addFavoriteMovie}
-              favoriteIcon={AddFavorites}
-            />
-          </div>
+            <div className='row d-flex align-items-center mt-3 mb-2'>
+                <MovieListHeader header='Search Here: '/>
+            </div>
+            <div className='row d-flex align-items-center mt-1 mb-4'>
+                <SearchBar searchKey='' setSearchKey={setSearchKey}/>
+            </div>
+
+            {/* Display Searching Results */}
+            {showSearchResultsHeader && (
+                <div>
+                    <div className='row d-flex align-items-center mt-3 mb-4'>
+                        <MovieListHeader header='Searching Results: '/>
+                    </div>
+                    <div className='row'>
+                        <MovieList
+                            movies={movies}
+                            handleFavoritesClick={addFavoriteMovie}
+                            favoriteIcon={AddFavorites}
+                        />
+                    </div>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default SearchPage;
